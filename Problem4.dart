b796d8e840d2967b5faf53713174745a15ef116d
snehalatha-appiness <113968257+snehalatha-appiness@users.noreply.github.com>
2022-09-20 12:41:46 +0530

    Update and rename Problem4 to Problem4.dart

--- Problem4.dart
+++ Problem4.dart
@@ -1,7 +1,7 @@
-Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
-The overall run time complexity should be O(log (m+n)).
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+// The overall run time complexity should be O(log (m+n)).
 
-Solution
+// Solution
 class Solution {
   double findMedianSortedArrays(List<int> nums1, List<int> nums2) {
  List<int> mergedArray = new List.from(nums1)..addAll(nums2);
